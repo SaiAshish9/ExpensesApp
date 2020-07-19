667038e3c47609e4140334acdce8763f167c8ac0
SaiAshish9 <saiashish3760@gmail.com>
2020-07-20 04:27:57 +0530

    added chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/transaction.dart';
+import 'widgets/chart.dart';
 import 'widgets/new_transaction.dart';
 import 'widgets/transaction_list.dart';
 
@@ -37,14 +38,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes  ', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 16.99,
-        date: DateTime.now()),
+    //   Transaction(
+    //       id: 't1', title: 'New Shoes  ', amount: 69.99, date: DateTime.now()),
+    //   Transaction(
+    //       id: 't2',
+    //       title: 'Weekly Groceries',
+    //       amount: 16.99,
+    //       date: DateTime.now()),
   ];
+
+
+List<Transaction> get _recentTransactions {
+  return _userTransactions.where(
+    (tx){
+          return tx.date.isAfter(
+            DateTime.now().subtract(Duration(days:7))
+          );
+    }
+  ).toList();
+}
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -89,16 +101,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
-            TransactionList(_userTransactions)
-          ],
+            Chart(_recentTransactions), 
+            TransactionList(_userTransactions)],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,7 +12,22 @@ class TransactionList extends StatelessWidget {
     return Container(
       height: 300,
       // ListView
-      child: ListView.builder(
+      child:
+      transactions.isEmpty?
+      Column(
+        children: <Widget>[
+          Text('No transactions added yet !',style:Theme.of(context).textTheme.title),
+          SizedBox(height:20),
+          Container(
+            height: 200,
+            child: Image.asset('assets/images/waiting.png',
+            fit :BoxFit.cover
+            ),
+          )
+        ]
+      ) 
+      :
+       ListView.builder(
         itemBuilder: (ctx, index) {
           return Card(
               child: Row(children: <Widget>[
